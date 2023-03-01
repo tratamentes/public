d49c01dc4b0a96dcbc4381c0343854d9e17e1f83
TrataMentes <tratamentes.pt@gmail.com>
2023-03-01 06:10:45 +0000

    formulario de envio de email

--- en/contact/form-contacte.php
+++ en/contact/form-contacte.php
@@ -3,13 +3,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $to_email = "joao.goulart@tratamentes.pt"; // Replace with your email address
     $subject = "Novo contacto :: " . $_POST["name"] . " :: tratamentes.pt"; // Replace with your desired email subject
-    $message = "Name: " . $_POST["name"] . "\nEmail: " . $_POST["email"] . "\nPhone: " . $_POST["phone"] . "\nSubject: " . $_POST["subject"] . "\nMessage: " . $_POST["message"]; // Format the email message
-
-    // Send the email
-    if (mail($to_email, $subject, $message)) {
-        echo "Thank you for contacting us!";
-    } else {
-        echo "There was an error sending your message. Please try again later.";
-    }
+    $messageform = "Name: " . $_POST["name"] . "\nEmail: " . $_POST["email"] . "\nPhone: " . $_POST["phone"] . "\nSubject: " . $_POST["subject"] . "\nMessage: " . $_POST["message"]; // Format the email message
 }
-?>
+// inclusão da framework no código
+require_once '../lib/swift_required.php';
+
+
+
+
+// definir a autenticação via SMTP
+// mail.dominios.pt -> deverá trocar pelo endereço de e-mail do seu domínio
+// webmaster@dominio.tld -> deverá trocar pelo seu endereço de e-mail
+// password_caixa_email -> deverá preencher com a password da respectiva caixa
+$transport = Swift_SmtpTransport::newInstance('mail.tratamentes.pt', 25)
+->setUsername('joao.goulart@tratamentes.pt')
+->setPassword('*5U7+fq8M@EE&V5T')
+;
+$mailer = Swift_Mailer::newInstance($transport);
+ 
+// Criar o cabeçalho, assim como a mensagem
+$message = Swift_Message::newInstance($subject)
+->setFrom(array('joao.goulart@tratamentes.pt' => 'Joao Goulart'))
+->setTo(array($to_email))
+->setBody($messageform)
+;
+// Efectuar o envio
+$result = $mailer->send($message);
